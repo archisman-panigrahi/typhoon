caeffebffd67e30c40b8fdd30220e1bec1fb45a4
Archisman Panigrahi <apandada1@gmail.com>
2013-04-16 22:19:26 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Typhoon
+=======
+
+- Typhoon is a simple weather application. It is and always will be free and open source.
+- Based on Stormcloud by Jono Cooper.
+- Thanks to Adam Whitcroft for Climacons.
+- Weather by Yahoo!.
+- Made with quickly.
+- Inspired by Richard M. Stallman, Linus Torvals, Mark Shuttleworth, Jono Cooper, Micheal Harker.
+- Designed, developed and maintained by Archisman Panigrahi.
+
+Contributers:
+--------------
+- Still none :(
---
+++ typhoon/__init__.py
@@ -0,0 +1,47 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2013 Archisman Panigrahi <apandada1@gmail.com>
+# Thanks to <adamwhitcroft.com> for Climacons!
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+import optparse
+
+import locale
+from locale import gettext as _
+locale.textdomain('typhoon')
+
+from gi.repository import Gtk # pylint: disable=E0611
+
+from typhoon import TyphoonWindow
+
+from typhoon_lib import set_up_logging, get_version
+
+def parse_options():
+    """Support for command line options"""
+    parser = optparse.OptionParser(version="%%prog %s" % get_version())
+    parser.add_option(
+        "-v", "--verbose", action="count", dest="verbose",
+        help=_("Show debug messages (-vv debugs typhoon_lib also)"))
+    (options, args) = parser.parse_args()
+
+    set_up_logging(options)
+
+def main():
+    'constructor for your class instances'
+    parse_options()
+
+    # Run the application.    
+    window = TyphoonWindow.TyphoonWindow()
+    window.show()
+    Gtk.main()
