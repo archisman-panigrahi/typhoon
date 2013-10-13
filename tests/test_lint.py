98258d6725c76fec257ec74ad57b876e88caee86
Zlatan Vasović <legospace9876@gmail.com>
2013-10-13 14:57:56 +0200

    Clean up comments in lint test

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -26,16 +26,14 @@ class TestPylint(unittest.TestCase):
         your code may well work even with pylint errors
         but have some unusual code'''
         return_code = subprocess.call(["pylint", '-E', 'typhoon'])
-        # not needed because nosetests displays pylint console output
-        #self.assertEqual(return_code, 0)
 
     # un-comment the following for loads of diagnostics   
-    #~ def test_project_full_report(self):
-        #~ '''Only for the brave
-#~ 
-        #~ you will have to make judgement calls about your code standards
-        #~ that differ from the norm'''
-        #~ return_code = subprocess.call(["pylint", 'typhoon'])
+    # def test_project_full_report(self):
+        # '''Only for the brave
+        #
+        # you will have to make judgement calls about your code standards
+        # that differ from the norm'''
+        # return_code = subprocess.call(["pylint", 'typhoon'])
 
 if __name__ == '__main__':
     'you will get better results with nosetests'
