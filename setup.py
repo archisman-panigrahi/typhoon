127f71e8255b2f6a8905fd6932fee0ac8c9d4e4f
apandada1 <apandada1@gmail.com>
2014-02-25 23:22:52 +0530

    Fix description

--- setup.py
+++ setup.py
@@ -142,11 +142,7 @@ DistUtilsExtra.auto.setup(
     author='Archisman Panigrahi',
     author_email='apandada1@gmail.com',
     description='Quickly check the weather with this beautiful application',
-    long_description='Typhoon is a free and open source weather application. It is continuation of discontinued Stormcloud 1.1 ,however with some changes. It is and always will be free.
-
-PPA: https://launchpad.net/~apandada1/+archive/typhoon
-
-Homepage: http://gettyphoon.tk/',
+    long_description='Typhoon is a free and open source weather application. It is continuation of discontinued Stormcloud 1.1 ,however with some changes. It is and always will be free.                                                                                                                                                  PPA: https://launchpad.net/~apandada1/+archive/typhoon/                                                                                                              Homepage: http://gettyphoon.tk/',
     url='https://launchpad.net/typhoon',
     cmdclass={'install': InstallAndUpdateDataDirectory}
     )
